f9b3aef5280a56891f31329aaaf8fd02a36e7bb9
luo0412 <1095847440@qq.com>
2022-05-12 14:34:36 +0000

    adjust

--- _directive/index.js
+++ _directive/index.js
@@ -4,111 +4,15 @@ import VueCompositionApi, {
   ref,
 } from "@vue/composition-api";
 import { watchDebounced, watchThrottled } from '@vueuse/core'
-
-// import "@picocss/pico/css/pico.classless.min.css"
 import "./style.less";
-
-import { api } from "./anno/@fetch";
-import { emojiUrls } from "./anno/@img";
-
 import unifiedParser from "./utils/unifiedParserUtil";
-
-const weatherApi = api.weather
-
-
-// const content = `
-
-// @img(https://luo0412.oss-cn-hangzhou.aliyuncs.com/static/images/index/xiong.gif) @nice
-// @img https://luo0412.oss-cn-hangzhou.aliyuncs.com/static/images/index/xiong.gif
-
-// `;
-
-const content = `#### 世界很大, 而我又是靓仔 @nice 
-
-虽然说了句正确的废话 @del 
-
-@dog @cat @tiger
-
-@doc https://procomponents.ant.design/components/editable-table
-
-@bvid BV1YT4y1Q7xx
-
-@abbr(HTML, "Hyper Text Markup Language")
-
-@weather
-
-@emoji{help} @emoji{java}
-
-\`\`\`
-@emoji{safe} 
-@emoji{ichange} 
-\`\`\`
-`
-
-const content3 = `# 世界很大, 而我又是靓仔 @nice    
-
----
-
-说点正确的废话 @del    
-
----
-
-- 相关链接 
-  - @code{title = coolma演示} https://github.com/ui-javascript/demo-coolma-202205
-  - @doc https://procomponents.ant.design/components/editable-table
-
----
-
-@emoji{xiong}
-@img ${emojiUrls.dog}
-@img("${emojiUrls.cat}"){style: "width: 150px;"}
-@emoji("${emojiUrls.cool}")
-@emoji{src: "${emojiUrls.tiger}"}
-
----
-
-A lovely language know as @abbr[namespace](HTML, "HTML的全称"){.red #id} @abbr(HTML, "HTML的全称"){.bg-blue.border-orange-lighter.border-solid}
-
----
-
-@weather
-
-@fetch("${weatherApi}"){includeKeys: '[*]'}
-
-@fetch{weather}
-
-@fetch{weather: true}
-
-数组属性没有引号包裹, 注解直接摆烂, 原地显示 --> @fetch{weather: true, includeKeys: ['date']}
-
-@fetch{weather: true, includeKeys: "['date']"}
-
-@fetch{weather: false}
-
----
-
-hello @nice
-  
-@nice hello
-
-hello hi @nice @nice
-
-hello hi *暂时跳过这种标签* @abbr(HTML, "HTML的全称"){.bg-blue} @nice @nice
-
-hello @nice @nice hi
-
-hello @nice test *em* @nice ssss *em* sss @nice xxx
-`
-
-
-
 
 const App = {
   template: `
     <div>
 
     <nav>
-      <button role="button" @click="writeFile">更新文件</button>
+      <button role="button" @click="writeFile">更新MD文件 + 关闭Tab页面</button>
     </nav>
 
     <main class="container-fluid">
@@ -156,8 +60,6 @@ const App = {
     return {
       before,
       after,
-      weatherApi,
-      emojiUrls,
       writeFile
     };
   },
@@ -170,8 +72,8 @@ Vue.config.productionTip = false;
 window.addEventListener('message', init, false)
 function init(event) {
     if (event.data.cmd === 'mountApp') {
-        window.$CONTENT = event.data.data // JSON内容
-        window.$MDPATH = event.data.mdPath // JSON路径
+        window.$CONTENT = event.data.data // MD内容
+        window.$MDPATH = event.data.mdPath // MD路径
   
         new Vue({
           el: "#app",
