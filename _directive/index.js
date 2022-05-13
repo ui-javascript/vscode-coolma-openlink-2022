f6355b463c41d95d31542fe9115716da8c50593c
luo0412 <1095847440@qq.com>
2022-05-13 02:45:15 +0000

    adjust

--- _directive/index.js
+++ _directive/index.js
@@ -4,16 +4,16 @@ import { watchDebounced, watchThrottled, useIntervalFn } from "@vueuse/core";
 import "./style.less";
 import unifiedParser from "./utils/unifiedParserUtil";
 
+// <button v-if="isActive" class="secondary outline" @click="pause">
+//   关闭自动刷新
+// </button>
+// <button v-if="!isActive" class="outline" @click="resume">
+//   开启自动刷新
+// </button>
+
 const App = {
   template: `
     <div>
-
-    <button v-if="isActive" class="outline" @click="pause">
-      关闭自动刷新
-    </button>
-    <button v-if="!isActive" class="secondary outline" @click="resume">
-      开启自动刷新
-    </button>
 
     <main class="container-fluid">
   
@@ -45,10 +45,11 @@ const App = {
       }
     );
 
-    const interval = ref(500)
-    const { pause, resume, isActive } = useIntervalFn(() => {
-      before.value = window.$CONTENT;
-    }, interval)
+    // const interval = ref(500)
+    // const { pause, resume, isActive } = useIntervalFn(() => {
+    //   debugger
+    //   before.value = window.$CONTENT;
+    // }, interval)
 
 
     // const writeFile = () => {
@@ -67,9 +68,9 @@ const App = {
       before,
       after,
       // writeFile,
-      pause, 
-      resume, 
-      isActive
+      // pause, 
+      // resume, 
+      // isActive
     };
   },
 };
@@ -93,11 +94,16 @@ const init = async (event) => {
   if (event.data.cmd === "mdSync") {
     window.$CONTENT = event.data.data; // MD内容
     window.$MDPATH = event.data.mdPath; // MD路径
+
+    if (window.$VUE) {
+      window.$VUE.$children[0].before = window.$CONTENT
+    } 
+    
   }
 
 };
 
-// window.$VUE = new Vue({
-//   el: "#app",
-//   render: (h) => h(App),
-// });
+window.$VUE = new Vue({
+  el: "#app",
+  render: (h) => h(App),
+});
