8b1e5c8dc1992e6213314bb58d06f7c24ae67f92
luo0412 <1095847440@qq.com>
2022-05-13 15:03:00 +0800

    adjust

--- _directive/index.js
+++ _directive/index.js
@@ -1,88 +1,55 @@
 import Vue from "vue";
-import VueCompositionApi, { onMounted, ref } from "@vue/composition-api";
-import { watchDebounced, watchThrottled, useIntervalFn } from "@vueuse/core";
+import VueCompositionApi, { onMounted, ref, watchEffect } from "@vue/composition-api";
+// import { watchDebounced, watchThrottled, useIntervalFn } from "@vueuse/core";
 import "./style.less";
 import unifiedParser from "./utils/unifiedParserUtil";
 
 
 
+
 const App = {
   template: `
-    <div>
 
-    <nav>
-      <button v-if="isActive" class="outline" @click="pause">
-        本地MD文件 自动预览中（{{interval}}ms）
-      </button>
-      <button v-if="!isActive" class="secondary outline" @click="resume">
-        开启 本地MD文件 自动预览
-      </button>
-    </nav> 
+  <main class="container-fluid">
 
+  <div class="grid">
 
-    <main class="container-fluid">
+    <div v-html="after"></div>
+
+  </div>
   
-    <div class="grid">
-
-      <textarea v-if="!isActive" style="display: block;height: 700px" v-model="before"></textarea>
-      <div style="min-height: 700px" v-html="after"></div>
-
-    </div>
-   
-    </main>
-    </div>
+  </main>
 
   `,
   setup() {
     const before = ref("");
     const after = ref("");
 
-    watchDebounced(
-      before,
-      async () => {
+    // 防抖已交给vscode插件实现
+    watchEffect(async () => {
         const res = await unifiedParser(before.value);
-
         console.log(String(res));
         after.value = String(res);
-      },
-      {
-        debounce: 200,
-        maxWait: 1000,
-      }
-    );
+    });
 
     onMounted(() => {
       before.value = window.$CONTENT
     })
 
-    const interval = ref(500)
-    const { pause, resume, isActive } = useIntervalFn(() => {
-      // console.log("定时器")
-      before.value = window.$CONTENT;
-    }, interval, {
-      immediate: false
-    })
-
-
-    const writeFile = () => {
-
-      window.parent.postMessage({
-        cmd: 'writeFile',
-        data: {
-            code: before.value,
-            mdPath: window.$MDPATH
-        }
-      }, '*')
-    }
+    // const writeFile = () => {
+    //   window.parent.postMessage({
+    //     cmd: 'writeFile',
+    //     data: {
+    //         code: before.value,
+    //         mdPath: window.$MDPATH
+    //     }
+    //   }, '*')
+    // }
 
     return {
       before,
       after,
-      writeFile,
-      pause, 
-      resume, 
-      isActive,
-      interval
+      // writeFile,
     };
   },
 };
@@ -92,19 +59,13 @@ Vue.use(VueCompositionApi);
 Vue.config.productionTip = false;
 
 window.addEventListener("message", init, false);
-const init = async (event) => {
+function init (event) {
 
-  // console.log(JSON.stringify(event))
-  // alert(JSON.stringify(event))
+  console.log(event)
 
   if (event.data.cmd === "mountApp") {
     window.$CONTENT = event.data.data; // MD内容
     window.$MDPATH = event.data.mdPath; // MD路径
-
-    // window.$VUE = new Vue({
-    //   el: "#app",
-    //   render: (h) => h(App),
-    // });
 
     window.$VUE = new Vue({
         render: h => h(App),
@@ -112,13 +73,15 @@ const init = async (event) => {
   
   }
 
-  if (event.data.cmd === "mdSync" || event.data.cmd === "changeContent") {
+  if (event.data.cmd === "mdSync") {
     window.$CONTENT = event.data.data; // MD内容
     window.$MDPATH = event.data.mdPath; // MD路径
-  }
 
+    if (window.$VUE) {
+      window.$VUE.$children[0].before = window.$CONTENT
+    } 
+  
+  }
+  
 };
 
-// window.$VUE = new Vue({
-//   render: h => h(App),
-// }).$mount('#app');
